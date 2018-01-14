a7e98d6af46668e5b7e171dfe10681fe22e35d40
hpreston <hank.preston@gmail.com>
2018-01-13 20:05:55 -0500

    Demo code complete v1

--- demo/push_configs.py
+++ demo/push_configs.py
@@ -1,28 +1,53 @@
+#! /usr/bin/env python
+"""
+Cisco Live BRKDEV-1368:
+  Effectively Understand and Leverage YANG with NETCONF and
+  RESTCONF for Model Drive Programmablity
+
+Demo: Model Driven Programmability in Action
+Authors: Hank Preston <hapresto@cisco.com>
+         Bryan Byrne <brybyrne@cisco.com>
+
+push_configs.py
+Illustrate the following concepts:
+- Storing configuration details in external (YAML) file and
+  loading dynamically into script
+- Creating NETCONF XML Templates with Jinja
+- Leveraging NETCONF and ncclient for pushing network
+  configuration to devices
+"""
+
+__author__ = ("Hank Preston", "Byran Byrne")
+__author_email__ = ("hapresto@cisco.com", "brybyrne@cisco.com")
+__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
+__license__ = "MIT"
 
 import yaml
 from jinja2 import Template
 from ncclient import manager
 import xmltodict
 
-# Load Configs
+# Load Network Config Details from YAML Config File
 print("Loading Network Configuration Details from YAML")
 with open("config_details.yaml") as f:
     config = yaml.load(f.read())
 
-# Create templates
+# Create Jinja Template Objects for NETCONF Payloads
 print("Setting Up NETCONF Templates")
+# Layer 3 Interface Configurations
 with open("layer3_interface_config.j2") as f:
     l3_template = Template(f.read())
 
+# OSPF Routing Configuration
 with open("ospf_config.j2") as f:
     ospf_template = Template(f.read())
 
 
-# Loop over devices
+# Loop over network devices to create and deploy network config
 print("Processing Device Configurations")
 for device in config["devices"]:
     print("Device: {}".format(device["name"]))
-    # Render Templates
+    # Create Device Specific Configurations
     print("  Creating Device Specific Configurations from Templates")
     with open("netconf_configs/{}_layer3.cfg".format(device["name"]), "w") as f:
         l3_config = l3_template.render(interfaces=device["interfaces"])
@@ -42,7 +67,7 @@ for device in config["devices"]:
                          allow_agent=False,
                          look_for_keys=False) as m:
 
-        # Send configurations
+        # Send NETCONF Configurations with <edit-config> RPC
         print("  Sending NETCONF Configuration edit-config operations")
         l3_reply = xmltodict.parse(m.edit_config(l3_config, target = "running").xml)
         ospf_reply = xmltodict.parse(m.edit_config(ospf_config, target = "running").xml)
