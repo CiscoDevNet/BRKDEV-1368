29faffc9222bd47f543b739528025a9acf317388
Bryan Byrne <brybyrne@cisco.com>
2019-01-11 07:25:00 -0500

    Netconf config examples

--- netconf/device_info.py
+++ netconf/device_info.py
@@ -1,18 +1,25 @@
 #! /usr/bin/env python
 """
-Learning Series: Network Programmability Basics
-Author: Hank Preston <hapresto@cisco.com>
+Copyright (c) 2019 Cisco and/or its affiliates.
 
-device_info.py
-Illustrate the following concepts:
-- Store device info for Sandbox Infrastructure used 
-  in examples
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.0 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
 """
 
-__author__ = "Hank Preston"
-__author_email__ = "hapresto@cisco.com"
-__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
-__license__ = "MIT"
+__author__ = "Hank Preston <hapresto@cisco.com>"
+__contributors__ = "Bryan Byrne <brybyrne@cisco.com>"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.0"
 
 # DevNet Always-On NETCONF/YANG & RESTCONF Sandbox Device
 # https://devnetsandbox.cisco.com/RM/Diagram/Index/27d9747a-db48-4565-8d44-df318fce37ad?diagramType=Topology
@@ -31,23 +38,3 @@ ios_xe2 = {
              "username": "root",
              "password": "cisco123"
           }
-
-
-# DevNet Always-On Sandbox APIC-EM
-# https://devnetsandbox.cisco.com/RM/Diagram/Index/2e0f9525-5f46-4f46-973e-0f0c1bf934fa?diagramType=Topology
-apicem = {
-             "host": "sandboxapicem.cisco.com",
-             "username": "devnetuser",
-             "password": "Cisco123!",
-             "port": 443
-         }
-
-# DevNet Always-On Sandbox ACI APIC
-# https://devnetsandbox.cisco.com/RM/Diagram/Index/5a229a7c-95d5-4cfd-a651-5ee9bc1b30e2?diagramType=Topology
-apic = {
-             "host": "https://sandboxapicdc.cisco.com",
-             "username": "admin",
-             "password": "ciscopsdt",
-             "port": 443
-         }
-
--- netconf/example4.py
+++ netconf/example4.py
@@ -24,31 +24,29 @@ __license__ = "Cisco Sample Code License, Version 1.0"
 
 from device_info import ios_xe1
 from ncclient import manager
-import xmltodict
-
-# NETCONF filter to use
-netconf_filter = open("filter-ietf-interfaces.xml").read()
 
 if __name__ == '__main__':
-    with manager.connect(host=ios_xe1["address"],
-                         port=ios_xe1["port"],
+
+    # NETCONF Config Template to use
+    netconf_template = open("config-temp-ietf-interfaces.xml").read()
+
+    # Build the XML Configuration to Send
+    netconf_payload = netconf_template.format(int_name="GigabitEthernet2",
+                                              int_desc="Configured by NETCONF",
+                                              ip_address="10.255.255.1",
+                                              subnet_mask="255.255.255.0"
+                                              )
+    print("Configuration Payload:")
+    print("----------------------")
+    print(netconf_payload)
+
+    with manager.connect(host=ios_xe1["address"], port=ios_xe1["port"],
                          username=ios_xe1["username"],
                          password=ios_xe1["password"],
                          hostkey_verify=False) as m:
 
-        # Get Configuration and State Info for Interface
-        netconf_reply = m.get(netconf_filter)
+        # Send NETCONF <edit-config>
+        netconf_reply = m.edit_config(netconf_payload, target="running")
 
-        # Process the XML and store in useful dictionaries
-        intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
-        intf_config = intf_details["interfaces"]["interface"]
-        intf_info = intf_details["interfaces-state"]["interface"]
-
-        print("")
-        print("Interface Details:")
-        print("  Name: {}".format(intf_config["name"]))
-        print("  Description: {}".format(intf_config["description"]))
-        print("  Type: {}".format(intf_config["type"]["#text"]))
-        print("  MAC Address: {}".format(intf_info["phys-address"]))
-        print("  Packets Input: {}".format(intf_info["statistics"]["in-unicast-pkts"]))
-        print("  Packets Output: {}".format(intf_info["statistics"]["out-unicast-pkts"]))
+        # Print the NETCONF Reply
+        print(netconf_reply)
